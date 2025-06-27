1cfde865bdc6499ecaf6556bc85769a87d08ac97
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2025-06-27 11:45:08 -0700

    Conexión a Firebase

--- PracticaAutenticacionYocupicioM/app/src/main/java/yocupicio/martin/practicaautenticacionyocupiciom/SignInActivity.kt
+++ PracticaAutenticacionYocupicioM/app/src/main/java/yocupicio/martin/practicaautenticacionyocupiciom/SignInActivity.kt
@@ -1,20 +1,32 @@
 package yocupicio.martin.practicaautenticacionyocupiciom
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        auth = Firebase.auth
+        val email: EditText = findViewById(R.id.etrEmail)
+        val password: EditText = findViewById(R.id.etrPassword)
+        val confirmPassword: EditText = findViewById(R.id.etrConfirmPassword)
+        val errorTv: TextView = findViewById(R.id.tvrError)
+
+        val button: Button = findViewById(R.id.btnRegister)
+
+        errorTv.visibility = View.INVISIBLE
     }
 }
