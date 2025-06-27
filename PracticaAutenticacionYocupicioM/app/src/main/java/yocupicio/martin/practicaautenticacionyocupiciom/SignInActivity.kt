0c4b9befd96777585a69d678555513687bdafa48
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2025-06-27 11:56:03 -0700

    Registro terminado

--- PracticaAutenticacionYocupicioM/app/src/main/java/yocupicio/martin/practicaautenticacionyocupiciom/SignInActivity.kt
+++ PracticaAutenticacionYocupicioM/app/src/main/java/yocupicio/martin/practicaautenticacionyocupiciom/SignInActivity.kt
@@ -1,10 +1,13 @@
 package yocupicio.martin.practicaautenticacionyocupiciom
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -28,5 +31,38 @@ class SignInActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.btnRegister)
 
         errorTv.visibility = View.INVISIBLE
+
+        button.setOnClickListener({
+            if(email.text.isEmpty() || password.text.isEmpty() || confirmPassword.text.isEmpty()){
+                errorTv.text = "Todos los campos deben ser llenados"
+                errorTv.visibility = View.VISIBLE
+            } else if(!password.text.toString().equals(confirmPassword.text.toString())){
+                errorTv.text = "Las contraseñas no coinciden"
+                errorTv.visibility = View.VISIBLE
+            } else {
+                errorTv.visibility = View.INVISIBLE
+                singIn(email.text.toString(), password.text.toString())
+            }
+        })
+    }
+
+    fun singIn(email: String, password: String){
+        Log.d("INFO", "email: ${email}, password: ${password}")
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
+            if(task.isSuccessful){
+                Log.d("INFO", "signInWithEmail:success")
+                val user = auth.currentUser
+                val intent = Intent(this, MainActivity::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            } else {
+                Log.w("ERROR", "signInWithEmail:failure", task.exception)
+                Toast.makeText(
+                    baseContext,
+                    "El registro falló.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
